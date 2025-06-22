7c14b2263f036643ed8f97fb305af2e0b24ef671
elhamhuq <elham22@vt.edu>
2025-06-22 17:45:28 -0400

    frontend and backend configuration

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 const coursesRouter = require('./routes/courses');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 
 app.use('/api/courses', coursesRouter);
